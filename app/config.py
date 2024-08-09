e7799f4b349c7853173106f60edfb431dac6780e
Qdavis24 <quoncdavis@gmail.com>
2024-08-09 13:37:27 -0600

    add deployment config

--- app/config.py
+++ app/config.py
@@ -3,5 +3,5 @@ import os
 
 class Config(object):
     SECRET_KEY = os.environ.get("SECRET_KEY")
-    SQLALCHEMY_DATABASE_URI = "sqlite:///database.db"
-    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DB_URI")
+    DEBUG = False
