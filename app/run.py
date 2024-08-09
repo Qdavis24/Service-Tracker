89d395bd10f9b5e2b78fa548510fabff29de60cb
Qdavis24 <quoncdavis@gmail.com>
2024-08-09 14:09:54 -0600

    configuring automatic database schema setup

--- app/run.py
+++ app/run.py
@@ -1,8 +1,9 @@
 from app import create_app
+from extensions import db
 
 app = create_app()
 
 if __name__ == "__main__":
     with app.app_context():
-        app.create_all()
+        db.create_all()
     app.run(debug=False)
